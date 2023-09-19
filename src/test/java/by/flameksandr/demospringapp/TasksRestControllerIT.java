cbd14182753953614cd5f0db75cc60c5a9d8e5fa
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-19 20:48:17 +0300

    integration test

---
+++ src/test/java/by/flameksandr/demospringapp/TasksRestControllerIT.java
@@ -0,0 +1,10 @@
+package by.flameksandr.demospringapp;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TasksRestControllerIT {
+
+}
