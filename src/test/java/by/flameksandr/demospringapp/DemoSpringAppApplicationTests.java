999b702667e4b9f5cb57c46c5806acdfcb4c353e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-19 17:00:50 +0300

    Initial commit

---
+++ src/test/java/by/flameksandr/demospringapp/DemoSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flameksandr.demospringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
