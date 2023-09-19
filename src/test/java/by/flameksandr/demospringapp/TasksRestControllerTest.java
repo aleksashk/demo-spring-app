6ee29ad475704ee344b7482c945d8b5ef09909ba
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-19 20:09:56 +0300

    create test handleCreateNewTask

--- src/test/java/by/flameksandr/demospringapp/TasksRestControllerTest.java
+++ src/test/java/by/flameksandr/demospringapp/TasksRestControllerTest.java
@@ -1,5 +1,6 @@
 package by.flameksandr.demospringapp;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,13 +9,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class TasksRestControllerTest {
@@ -29,6 +33,7 @@ class TasksRestControllerTest {
     TasksRestController controller;
 
     @Test
+    @DisplayName("GET /api/tasks/ возвращает HTTP-ответ со статусом 200 и списком задач")
     void handleGetAllTasks_ReturnsValidResponseEntity() {
         //given
         var tasks = List.of(
@@ -48,12 +53,28 @@ class TasksRestControllerTest {
     }
 
     @Test
-    void handleCreateNewTask() {
+    void handleCreateNewTask_PayloadIsValid_ReturnsValidResponseEntity() {
         //given
+        var details = "Third task";
 
         //when
-
+        var responseEntity = this.controller.handleCreateNewTask(new NewTaskPayload(details),
+                UriComponentsBuilder.fromUriString("http://localhost:8080"), Locale.ENGLISH);
         //then
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
+        if (responseEntity.getBody() instanceof Task task) {
+            assertNotNull(task.id());
+            assertEquals(details, task.details());
+            assertFalse(task.completed());
+
+            assertEquals(URI.create("http://localhost:8080/api/tasks/" + task.id()),
+                    responseEntity.getHeaders().getLocation());
+
+            verify(this.taskRepository).save(task);
+        }
+
     }
 
     @Test
