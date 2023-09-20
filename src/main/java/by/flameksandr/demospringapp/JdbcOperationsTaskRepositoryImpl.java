75ee58629832670106b57113f7a049d524c0a4c1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-20 12:11:02 +0300

    implements RowMapper<Task>, add method mapRow fill methods getTasks, findById

--- src/main/java/by/flameksandr/demospringapp/JdbcOperationsTaskRepositoryImpl.java
+++ src/main/java/by/flameksandr/demospringapp/JdbcOperationsTaskRepositoryImpl.java
@@ -1,14 +1,17 @@
 package by.flameksandr.demospringapp;
 
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public class JdbcOperationsTaskRepositoryImpl implements TaskRepository {
+public class JdbcOperationsTaskRepositoryImpl implements TaskRepository, RowMapper<Task> {
 
     private final JdbcOperations jdbcOperations;
 
@@ -18,7 +21,7 @@ public class JdbcOperationsTaskRepositoryImpl implements TaskRepository {
 
     @Override
     public List<Task> getTasks() {
-        return null;
+        return this.jdbcOperations.query("select * from t_task", this);
     }
 
     @Override
@@ -30,6 +33,15 @@ public class JdbcOperationsTaskRepositoryImpl implements TaskRepository {
 
     @Override
     public Optional<Task> findById(UUID id) {
-        return Optional.empty();
+        return this.jdbcOperations.query("select * from t_task where id = ?",
+                new Object[]{id},
+                this).stream().findFirst();
+    }
+
+    @Override
+    public Task mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Task(rs.getObject("id", UUID.class),
+                rs.getString("c_details"),
+                rs.getBoolean("c_completed"));
     }
 }
